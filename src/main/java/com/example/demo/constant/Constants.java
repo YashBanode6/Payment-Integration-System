426f1507b9bdf1d5e7a8ae0122080a5d276b4768
YashB <ybanode@ssdbtech.com>
2026-03-21 18:32:12 +0530

    Updating constants

--- src/main/java/com/example/demo/constant/Constants.java
+++ src/main/java/com/example/demo/constant/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
 	public static final String SUCCESS_URL = "success_url";
 	public static final String MODE_PAYMENT = "payment";
 	public static final String MODE = "mode";
+	public static final String ID = "id";
 	
 	
 
